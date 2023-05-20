38c3159f7855334a01586817b7c361c7c87fd6ef
carolinevsboliveira <carolinevsboliveira@gmail.com>
2023-05-20 18:36:53 -0300

    feat: add navbar styles

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,7 +1,9 @@
 import { client } from "~/helpers/requests/client";
-import { ClickableLinks } from "./clicable-navbar/clicable-navbar";
+import { ClickableLinks } from "./clicable-items";
 import { NavbarResponse } from "./entities";
 import style from "./style.module.scss";
+import { Icon } from "../desing/font-awnsome-icons";
+
 export const fetchNavbarProps = async () => {
   const result = await client.get<NavbarResponse>("api/homes", {
     params: {
@@ -20,23 +22,41 @@ export async function Navbar() {
   // const navProps = await fetchNavbarProps();
   const navProps = [
     { title: "ah", id: "1", link: "/" },
-    { title: "ahb", id: "2", link: "/contact" },
+    { title: "ah", id: "1", link: "/" },
   ];
 
   return (
-    <div className={style["header"]}>
-      <nav>
-        <ul>
-          <li>
-            {/* {navProps.navbar.navItems.map(({ title, id, link }) => (
-              <ClickableLinks linkName={title} href={link ?? "#"} key={id} />
-            ))} */}
-            {navProps.map(({ title, id, link }) => (
-              <ClickableLinks linkName={title} href={link ?? "#"} key={id} />
-            ))}
+    // <div className={style["header"]}>
+    //   <nav>
+    //     <ul>
+    //       <li>
+    //         {/* {navProps.navbar.navItems.map(({ title, id, link }) => (
+    //           <ClickableLinks linkName={title} href={link ?? "#"} key={id} />
+    //         ))} */}
+    //         {navProps.map(({ title, id, link }) => (
+    //           <ClickableLinks linkName={title} href={link ?? "#"} key={id} />
+    //         ))}
+    //       </li>
+    //     </ul>
+    //   </nav>
+    // </div>
+    <nav className={style["navbar"]}>
+      <div className={style["logo"]}>MUO</div>
+      <input
+        type="checkbox"
+        id={style["checkbox_toggle"]}
+        style={{ display: "none" }}
+      />
+      <label htmlFor={style["checkbox_toggle"]} className={style["hamburger"]}>
+        <Icon use="plusSquare" />
+      </label>
+      <ul className={style["nav-links"]} id={style["menu"]}>
+        {navProps.map(({ title, id, link }) => (
+          <li key={id}>
+            <ClickableLinks linkName={title} href={link ?? "#"} key={id} />
           </li>
-        </ul>
-      </nav>
-    </div>
+        ))}
+      </ul>
+    </nav>
   );
 }
