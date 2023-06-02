d57ee2a9f8e26f375d660c21e1a985cbeabd91f6
carolinevsboliveira <carolinevsboliveira@gmail.com>
2023-06-02 16:08:52 -0300

    chore: add eslint rules

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: process.env.BASE_ASSETS_API,
+        port: process.env.BASE_ASSETS_PORT,
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
