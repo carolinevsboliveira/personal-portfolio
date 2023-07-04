60b431021cdd0880d2d60a8d6e74f75fb4886df7
carolinevsboliveira <carolinevsboliveira@gmail.com>
2023-07-03 23:09:58 -0300

    chore: add underlined link

--- src/components/ui/about-me-section/about-me-section.tsx
+++ src/components/ui/about-me-section/about-me-section.tsx
@@ -4,10 +4,17 @@ import Image from "next/image";
 
 export async function AboutMeSection() {
   const aboutMeProps = await fetchAboutMeSection();
+  const mainTitle = aboutMeProps.aboutMe.mainTitle;
+  const personalPhrase = aboutMeProps.aboutMe.mainPhrase;
   const imageAtributtes = aboutMeProps.aboutMe.profilePhoto.data.attributes;
   return (
     <div className={style["main-information"]}>
-      <div className={style["presentation-text"]}>Ahh</div>
+      <div className={style["presentation-text"]}>
+        <p>{mainTitle.title}</p>
+        <h2>
+          <span>{personalPhrase}</span>
+        </h2>
+      </div>
       <Image
         src={`${process.env.BASE_API_URL}${imageAtributtes.url}`}
         alt={imageAtributtes.alternativeText ?? "Profile image"}
